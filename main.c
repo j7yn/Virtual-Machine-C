c3fd1e2733e649f6e707bf2f9ed18756923c78b0
Hajin Jeong <jhajin273@gmail.com>
2025-06-13 19:53:50 -0700

    fixed comment alignment main.c

--- main.c
+++ main.c
@@ -8,31 +8,31 @@ static int stack[STACK_SIZE];
 
 // Instructions
 typedef enum {
-	HLT, // 0	- hlt				:= halts program
-	PSH, // 1	- psh val			:= pushes <val> to stack
-	POP, // 2	- pop				:= pops value from stack
-	ADD, // 3   - add				:= adds top two vals on stack
-	MUL, // 4   - mul				:= multiplies top two vals on stack
-	DIV, // 5   - div				:= divides top two vals on stack
-	SUB, // 6   - sub				:= subtracts top two vals on stack 
+	HLT, // 0   - hlt		:= halts program
+	PSH, // 1   - psh val		:= pushes <val> to stack
+	POP, // 2   - pop		:= pops value from stack
+	ADD, // 3   - add		:= adds top two vals on stack
+	MUL, // 4   - mul		:= multiplies top two vals on stack
+	DIV, // 5   - div		:= divides top two vals on stack
+	SUB, // 6   - sub		:= subtracts top two vals on stack 
 	SLT, // 7   - slt reg_a, reg_b	:= pushes (reg_a < reg_b) to stack
 	MOV, // 8   - mov reg_a, reg_b 	:= moves the value in reg_a to reg_b
-	SET, // 9   - set reg, val		:= sets the reg to value
-	LOG, // 10  - log a				:= prints out a
-	IF,  // 11  - if reg val ip		:= if the register == val branch to the ip
+	SET, // 9   - set reg, val	:= sets the reg to value
+	LOG, // 10  - log a		:= prints out a
+	IF,  // 11  - if reg val ip	:= if the register == val branch to the ip
 	IFN, // 12  - ifn reg val ip	:= if the register != val branch to the ip
-	GLD, // 13  - gld reg			:= loads a register to the stack
-	GPT, // 14  - gpt reg			:= pushes top of stack to the given register
-	NOP  // 15  - nop				:= nothing
+	GLD, // 13  - gld reg		:= loads a register to the stack
+	GPT, // 14  - gpt reg		:= pushes top of stack to the given register
+	NOP  // 15  - nop		:= nothing
 } Instructions;
 
 // Registers
 typedef enum {
 	A, B, C, D, E, F, I, J, // General Purpose
-	EX,						// Excess 
-	EXA,					// More Excess
-	IP,						// Instruction Pointer
-	SP,						// Stack Pointer
+	EX,			// Excess 
+	EXA,			// More Excess
+	IP,			// Instruction Pointer
+	SP,			// Stack Pointer
 	REGISTER_SIZE
 } Registers;
 
